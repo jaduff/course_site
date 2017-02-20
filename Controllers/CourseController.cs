741456f37d8b31abb20175c1d600d532b6474ac9
James Duff <jaoduff@gmail.com>
2017-02-20 21:54:36 +0800

    Implementing course view, error in use of @model

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using course_site.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace course_site.Controllers
+{
+    public class CourseController : Controller
+    {
+
+        private CourseModel courseModel;
+
+        public CourseController()
+        {
+            courseModel = new CourseModel();
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(courseModel.CourseList);
+        }
+    }
+}
---
+++ ViewModels/CourseViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using course_site.Models;
+
+namespace course_site.ViewModels
+{
+    public class CourseViewModel
+    {
+        public CourseModel courseModel { get;}
+
+        public CourseViewModel()
+        {
+            courseModel = new CourseModel();
+        }
+    }
+}
